9fa426291c87ae9be108bc2b31023772f47d259e
borgoo <borgo-ale@libero.it>
2025-11-19 17:06:49 +0100

    feat: implement FisherYates bag
    - Add tests for FisherYates
    - Update README.md

---
+++ FisherYates/FisherYates.cs
@@ -0,0 +1,76 @@
+﻿namespace FisherYates;
+
+public class FisherYates : IFisherYatesBag
+{
+    private int _remainingCount;
+    private readonly int _min;
+    private readonly int _max;
+    private readonly int[] _possibleValues;
+
+    public int RemainingCount => _remainingCount;
+
+
+    public FisherYates(int min, int max)
+    {
+        if (max < min) throw new ArgumentException($"{nameof(max)} must be greater or equal to {nameof(min)}.");
+
+        _min = min;
+        _max = max;
+        int count = _max - _min + 1;
+        _possibleValues = new int[count];
+        _remainingCount = count;
+
+        for (int i = 0; i < count; i++) _possibleValues[i] = _min + i;
+    }
+
+    public FisherYates(int max) : this(0, max) { }
+
+
+    public bool TryGetNextRandom(out int? value) {
+        value = null;
+        if (_remainingCount <= 0) return false;
+        value = GetNextRandom();
+        return true;
+
+    }
+
+    public int GetNextRandom()
+    {
+        if(_remainingCount <= 0) throw new InvalidOperationException("No more unique random numbers available.");
+        int randomIndex = Random.Shared.Next(0, _remainingCount);
+        int value = _possibleValues[randomIndex];
+        int lastPosition = _remainingCount - 1;
+        (_possibleValues[randomIndex], _possibleValues[lastPosition]) = (_possibleValues[lastPosition], _possibleValues[randomIndex]);
+        _remainingCount--;
+        return value;
+
+    }
+
+    public void Reset()  => _remainingCount = _possibleValues.Length;
+
+    public IEnumerable<int> GetAllRandomized()
+    {
+        if(_remainingCount < _possibleValues.Length) Reset();
+        return GetAllRemainingRandomized();       
+    }
+
+    public IEnumerable<int> GetAllRemainingRandomized()
+    {
+        if (_remainingCount <= 0) throw new InvalidOperationException("No more unique random numbers available.");
+        List<int> results = new(_remainingCount);
+        while (_remainingCount > 0) results.Add(GetNextRandom());
+        return results;
+    }
+
+    public IEnumerable<int> ExtractKRandomNumbers(int k)
+    {
+        if(k > _remainingCount) throw new ArgumentException($"Too many numbers requested.");
+        List<int> results = new(k);
+        while (k > 0)
+        {
+            results.Add(GetNextRandom());
+            k--;
+        }
+        return results;
+    }
+}
---
+++ FisherYates/IFisherYatesBag.cs
@@ -0,0 +1,48 @@
+﻿namespace FisherYates;
+
+public interface IFisherYatesBag
+{
+    /// <summary>
+    /// Just update the RemainingCount to the total number of possible values.
+    /// (WARNING: this method doesn't reset the possible values array to the initial state!)
+    /// </summary>
+    public void Reset();
+
+
+    /// <summary>
+    /// Extract k random numbers from the bag.
+    /// </summary>
+    /// <param name="k">The number of numbers to extract</param>
+    /// <returns>The extracted numbers</returns>
+    public IEnumerable<int> ExtractKRandomNumbers(int k);
+
+    /// <summary>
+    /// Get the next random number from the bag.
+    /// </summary>
+    /// <returns></returns>
+    public int GetNextRandom();
+
+    /// <summary>
+    /// Try to get the next random number from the bag.
+    /// </summary>
+    /// <param name="value">The next random number from the bag</param>
+    /// <returns>true if a value was found, false otherwise</returns>
+    public bool TryGetNextRandom(out int? value);
+
+    /// <summary>
+    /// Reset the bag and return all the possible values in a random order (consuming the bag).
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerable<int> GetAllRandomized();
+
+    /// <summary>
+    /// Return all reamining possible values from the bag in a random order (consuming the bag).
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerable<int> GetAllRemainingRandomized();
+
+    /// <summary>
+    /// Get the number of remaining possible values in the bag.
+    /// </summary>
+    public int RemainingCount { get; }
+}
